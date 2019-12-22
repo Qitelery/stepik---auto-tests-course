554b4a76759857745ef143387ffb2b0a84bc5a16
Alexey Dronov <alexadronov@gmail.com>
2019-12-22 15:46:26 +0300

    lesson36_exercise2_method

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_items(browser):
+    browser.get(link)
+    time.sleep(30)
+    browser.find_element_by_class_name("btn btn-lg btn-primary btn-add-to-basket")
